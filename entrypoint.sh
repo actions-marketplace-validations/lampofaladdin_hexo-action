e07eb0230aa50469bf4e30a2b41bd3a2ee425b9c
aladdin <1295106869@qq.com>
2024-04-22 17:16:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,26 @@
 
 set -e
 
+echo "set key"
 # setup ssh-private-key
 mkdir -p /root/.ssh/
 echo "$INPUT_DEPLOY_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 
+echo "set git"
 # setup deploy git account
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
+echo "set hexo-cli"
 # install hexo env
 npm install hexo-cli -g
+
+echo "set hexo-cli-git"
 npm install hexo-deployer-git --save
 
+echo "set file permission"
 chown -R `whoami` /usr/local/lib/node_modules
 
 # deployment
